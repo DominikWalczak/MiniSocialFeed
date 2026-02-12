6cd949ba423ce3bb61fc11374a064ed247d598e5
Dominik Walczak <dominik.walczak2@gmail.com>
2026-02-12 07:21:10 +0100

    usuwam use client z layoutu, poprawiam QueryFetch oraz CSS w PostItem.tsx

--- app/components/PostItem.tsx
+++ app/components/PostItem.tsx
@@ -9,7 +9,7 @@ const PostItem = ({ data }: { data: PostItemType }) => {
   return (
     <div className="bg-[rgb(8,81,156)] text-[rgb(236,231,242)] w-xl flex flex-col items-center justify-center p-10 gap-3 rounded-3xl">
       {data.map((post) => (
-        <div key={post.id} className="w-full bg-[rgb(33,113,181)] rounded-3xl shadow-[rgb(33,113,181) p-6">
+        <div key={post.id} className="w-full bg-[rgb(33,113,181)] rounded-3xl shadow-[rgb(33,113,181)] p-6">
           <div>{t('AuthorsId')}: {post.authorId}</div>
           <div>{t('CreatedAt')}: {post.createdAt}</div>
           <div>{t('PostContent')}: {post.content}</div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import "./globals.css";
 import "../src/i18n/i18n";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
