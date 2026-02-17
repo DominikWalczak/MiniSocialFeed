b5978c696395d91e6f0999ba158b75ccacb8aaf3
Dominik Walczak <dominik.walczak2@gmail.com>
2026-02-17 16:30:25 +0100

    Dodaję providers.tsx oraz naprawiam layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,13 @@
 import "./globals.css";
 import "../src/i18n/i18n";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Providers } from './providers';
 
-const queryClient = new QueryClient()
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    // Wrapuję aplikację QueryClientProvider, inaczej useQuery nie będzie działać
-    <QueryClientProvider client={queryClient}>
-      <html className="m-3" lang="en">
-        <body>
-          {children}
-        </body>
-      </html>
-    </QueryClientProvider>
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
   );
 }
